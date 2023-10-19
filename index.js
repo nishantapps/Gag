572ea0f7a083c50b535177ca000802cea5265bc4
Nishant Sharma <nishantapps55@gmail.com>
2023-10-19 20:10:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
 const express = require("express");
 const hero = require("./scrapeLogic");
 const app = express();
-
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const PORT = process.env.PORT || 1000;
 
 app.get("/scrape", (req, res) => {
   if(!req.headers['text']){
                 return res.json({response:"Text header is missing"});
             }
-  hero.getImages(res,req.headers['text'])
+  puppeteer.use(StealthPlugin());
+
+  const browser = await puppeteer.launch({
+    headless:true,
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+
+  // Create a new page
+  const page = await browser.newPage();
+
+  // Go to the Cloudflare-protected website
+  await page.goto('https://www.craiyon.com/');
+
+  // Wait for the page to load
+  await page.waitForNavigation();
+
+  const text = '#prompt';
+try{
+await page.waitForSelector(text);
+}catch(err){
+    console.log(err);
+}
+await page.type(text, prompt);
+await page.click('#generateButton');
+await page.waitForTimeout(90000);
+  const selector = `img[alt="${prompt}"]`;
+  await page.waitForSelector(selector, {
+    timeout: 12e4
+  });
+  const imgs = await page.$$eval(selector, (imgs2) => imgs2.map((img) => img.getAttribute("src")));
+  await browser.close();
+  res.json({ response: imgs });
   
 });
 app.get('/images/:image', (req, res) => {
