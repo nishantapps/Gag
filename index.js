b5cf7ef0c1cb46f8c65176dc2a8b015583877408
Nishant Sharma <nishantapps55@gmail.com>
2023-11-18 16:09:50 +0530

    Update index.js (#9)

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
+const puppeteer = require('puppeteer');
 const express = require('express');
-const puppeter = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeter.use(StealthPlugin());
-const app = express();
 
-app.get('/', async (req, res) => {
-        if (!req.headers.prompt) return res.status(400).json({ error: 'Prompt not found' });
-    const browser = await puppeter.launch({ headless:true,
+async function scrape(text){
+    const browser = await puppeteer.launch({ 
+        headless:true,
         args: [
           "--disable-setuid-sandbox",
           "--no-sandbox",
@@ -16,39 +13,34 @@ app.get('/', async (req, res) => {
         executablePath:
           process.env.NODE_ENV === "production"
             ? process.env.PUPPETEER_EXECUTABLE_PATH
-            : puppeteer.executablePath(),});
+            : puppeteer.executablePath(),
+    });
     const page = await browser.newPage();
-    await page.goto('https://deepai.org/machine-learning-model/text2img', { waitUntil: 'networkidle2' ,timeout:60000000});
-    const textbox = 'textarea[class="model-input-text-input"]';
-    await page.waitForSelector(textbox, {timeout:60000000});
-    await page.type(textbox, req.headers.prompt, );
-    await page.click('button[id="modelSubmitButton"]', {timeout:60000000});
-    await page.waitForTimeout(10000);
-    const img = await page.$eval('div[class="try-it-result-area"]', el => el.querySelector('img').src);
-    res.json({ img: img });
-    browser.close();
-});
-app.get('/preview/:prompt', async (req, res) => {
-    try{
-        const browser = await puppeter.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto('https://deepai.org/machine-learning-model/text2img', { waitUntil: 'networkidle2' });
-    const textbox = 'textarea[class="model-input-text-input"]';
-    await page.waitForSelector(textbox);
-    await page.type(textbox, req.params.prompt);
-    await page.click('button[id="modelSubmitButton"]');
-    await page.waitForTimeout(10000);
-    const img = await page.$eval('div[class="try-it-result-area"]', el => el.querySelector('img').src);
-    //download image in temp
-    const viewSource = await page.goto(img);
-    const buffer = await viewSource.buffer();
-    res.set('Content-Type', 'image/png');
-    res.send(buffer);
-    browser.close();
-    }catch(err){
-        res.json({error:err})
-    }
-});
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+    
+    // Navigate to the page that will perform the tests.
+    await page.goto('https://dalle-mini-dalle-mini.static.hf.space/index.html');
+    
+   //type
+    await page.waitForSelector('input[type="text"]',{timeout: 600000});
+    await page.type('input[type="text"]', text);
+    await page.click('.gr-button')
+    //get all images from a div named "images"
+    await page.waitForSelector('div.grid-cols-3 button img',{timeout: 600000});
+    const images = await page.$$eval('div.grid-cols-3 button img', imgs => imgs.map(img => img.src),{timeout: 600000});
+    await browser.close();
+    return images;   
+}
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+    const text = req.headers.text;
+        if(!text) return res.json({error:"No prompt found"})
+    const images = await scrape(text);
+    res.json({images_response:images});
+}
+);
+
+app.listen(1000, () => console.log('Server ready'))
